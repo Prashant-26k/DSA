45027d711f2d109fab7266f318857b910cf1a6fc
Prashant-26k <143924282+Prashant-26k@users.noreply.github.com>
2026-08-22 13:46:15 +0530

    Solved: Longest Repeating Character Replacement [Medium]

---
+++ Strings/Medium/Longest Repeating Character Replacement/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> freq = new HashMap<>();
+        int answer = 0, i = 0, maxFreq = 0;
+
+        for(int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            freq.put(c, freq.getOrDefault(c,0) + 1);
+            maxFreq = Math.max(maxFreq, freq.get(c));
+
+            while((j - i + 1) - maxFreq > k) {
+                char left = s.charAt(i);
+                freq.put(left, freq.get(left) - 1);
+                i++;
+            }
+
+            answer = Math.max(answer, j - i + 1);
+        }
+        return answer;
+    }
+}
