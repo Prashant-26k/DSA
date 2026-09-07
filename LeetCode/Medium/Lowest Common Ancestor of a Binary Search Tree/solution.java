47cdee7cc5bbd27c96dd291703bf2cba8455fa68
Prashant Kharkwal <143924282+Prashant-26k@users.noreply.github.com>
2026-09-07 21:11:20 +0530

    Solved: Lowest Common Ancestor of a Binary Search Tree [Medium]

---
+++ LeetCode/Medium/Lowest Common Ancestor of a Binary Search Tree/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+
+        if(root == p || root == q) return root;
+
+        TreeNode left = new TreeNode(), right = new TreeNode();
+
+        if(root.val < p.val && root.val < q.val){
+           left = lowestCommonAncestor(root.right, p, q); 
+           right = null;
+        }
+        else if(root.val > p.val && root.val > q.val){
+            right = lowestCommonAncestor(root.left, p, q);
+            left = null;
+        }
+        // else {
+        //     left = lowestCommonAncestor(root.left, p, q);
+        //     right = lowestCommonAncestor(root.right, p, q);
+        // }
+        
+        if(left != null && right != null) return root;
+        else if(left == null && right != null) return right;
+        else return left;
+    }
+}
